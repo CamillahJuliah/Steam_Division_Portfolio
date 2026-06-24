2b74c7e35fc76897d8fc9746d542b009ba511a6d
Camsu <143175923+CamillahJuliah@users.noreply.github.com>
2026-06-24 23:55:48 +0300

    Silmän muotoilu & korjailu

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+let breatheTime = 0;
+let blinking = false;
+let blinkScale = 1;
+
 const eye = document.querySelector(".eye");
 const iris = document.querySelector(".iris");
 
@@ -23,12 +27,40 @@ document.addEventListener("mousemove", function (event) {
 });
 
 function animateEye() {
+    breatheTime += 0.03;
+
+    const microX = Math.sin(breatheTime) * 0.8;
+    const microY = Math.cos(breatheTime * 0.8) * 0.5;
+
     currentX += (targetX - currentX) * 0.12;
     currentY += (targetY - currentY) * 0.12;
 
-    iris.style.transform = `translate(${currentX}px, ${currentY}px) scale(1.08)`;
+    iris.style.transform =
+        `translate(${currentX + microX}px, ${currentY + microY}px) scale(1.08)`;
 
     requestAnimationFrame(animateEye);
 }
 
+function blink() {
+    if (blinking) return;
+
+    blinking = true;
+    eye.classList.add("blink");
+
+    setTimeout(() => {
+        eye.classList.remove("blink");
+        blinking = false;
+    }, 160);
+}
+
+function randomBlink() {
+    blink();
+
+    const nextBlink = Math.random() * 5000 + 3000;
+
+    setTimeout(randomBlink, nextBlink);
+}
+
 animateEye();
+setTimeout(randomBlink, 3000);
+
